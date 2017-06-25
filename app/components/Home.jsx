541792c263d7a540add28c8c72291440f459cb74
Zach Cossman <Zachcos@gmail.com>
2017-06-25 19:52:07 -0400

    Add initial axios call to Reddit API

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 export class Home extends React.Component {
+  componentDidMount() {
+    axios
+      .get('http://www.reddit.com/r/all.json')
+      .then(({ data }) => {
+        console.log(data)
+      })
+  }
+
   render() {
     return <h3>This is the home component</h3>
   }
