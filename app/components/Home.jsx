1cd2abe96e90607470eea92b39e82e0a9f5ac095
Zach Cossman <Zachcos@gmail.com>
2017-06-25 19:48:34 -0400

    Rewrite Home component as class declaration

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-export const Home = () =>
-  <h3>This is the home component</h3>
+export class Home extends React.Component {
+  render() {
+    return <h3>This is the home component</h3>
+  }
+}
 
 export default Home;
