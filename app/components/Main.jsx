e92673a40753835e5cc00d2e7162d895b98fb2d7
Zach Cossman <Zachcos@gmail.com>
2017-06-25 12:40:33 -0400

    Create Navigation component and set initial styles

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import Navigation from 'Navigation';
 
-const Main = () => 
-  <h3>This is the main component</h3>
+export class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <h3>This is the main component</h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Main;
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () =>
+  <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+      <div className="navbar-left">
+        <a className="navbar-brand" href="#">Reedit</a>
+      </div>
+    </div>
+  </nav>
+
+export default Navigation;
