5dbfed6d4b49a56a5680e59118e9ed4dc5bbde0e
Zach Cossman <Zachcos@gmail.com>
2017-06-25 19:54:04 -0400

    Set retrieved data to app state

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import axios from 'axios';
 
 export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      kind: '',
+      data: []
+    };
+  }
+
   componentDidMount() {
     axios
-      .get('http://www.reddit.com/r/all.json')
-      .then(({ data }) => {
-        console.log(data)
+    .get('http://www.reddit.com/r/all.json')
+    .then(({ data }) => {
+      this.setState({
+        kind: data.kind,
+        data: data.data.children
       })
+    })
   }
 
   render() {
