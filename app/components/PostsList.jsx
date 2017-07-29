88859ce182e8df9f27662a023780a9a8a061d54d
Zach Cossman <Zachcos@gmail.com>
2017-07-29 12:08:55 -0400

    Add styles to mark over-18 posts in red

--- app/components/PostsList.jsx
+++ app/components/PostsList.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const PostsList = ({ posts }) => {
   const renderPosts = () => {
+    let bgColor = '';
     return posts.map(item => {
+      if (item.data.over_18) {
+        bgColor = 'crimson'
+      } else {
+        bgColor = 'lightblue'
+      }
+
       return (
-        <div className="panel panel-default" key={item.data.id}>
+        <div className="panel panel-default" style={{backgroundColor: bgColor}} key={item.data.id}>
           <div className="panel-body">
             {item.data.title}
           </div>
