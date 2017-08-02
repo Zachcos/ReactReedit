62195d182a102ab6bbbdb38ca51dfb16fde7d7e1
Zach Cossman <Zachcos@gmail.com>
2017-08-02 02:10:56 -0400

    Add post author and link to reddit user page

--- app/components/PostsList.jsx
+++ app/components/PostsList.jsx
@@ -12,7 +12,7 @@ const PostsList = ({ posts }) => {
 
       return (
         <div className="panel panel-default" style={{backgroundColor: bgColor}} key={item.data.id}>
-          <div className="panel-body container-fluid">
+          <div className="panel-body">
             <div style={{display: 'inline'}}>
               <span className="glyphicon glyphicon-arrow-up">
               </span><b style={{marginLeft: 5}}>{item.data.ups}</b>
@@ -21,6 +21,8 @@ const PostsList = ({ posts }) => {
               {item.data.title}
             </div>
           </div>
+          <hr />
+            <p style={{marginLeft: 10}}>Post by: <b><a href={`http://www.reddit.com/u/${item.data.author}`} target="_blank">{item.data.author}</a></b></p>
         </div>
       )
     }
